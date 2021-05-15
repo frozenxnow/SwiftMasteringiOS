fd0db82203ad41c8d9256ad0c47ffe04ff3107d2
frozenxnow <frozenxnow@naver.com>
2021-05-15 23:54:40 +0900

    2021-05-15

--- Mastering-iOS-master/xcode12+/Date/Date.playground/Pages/DateType.xcplaygroundpage/Contents.swift
+++ Mastering-iOS-master/xcode12+/Date/Date.playground/Pages/DateType.xcplaygroundpage/Contents.swift
@@ -27,10 +27,14 @@ import UIKit
  
  ### Reference date: 2001-01-01 00:00:00 / UTC
  */
+let now = Date()
+print(now)
 
+var dt = Date(timeIntervalSinceReferenceDate: 60 * 60)
+print(dt)
 
-
-
+var dt = Date(timeIntervalSinceReferenceDate: -60 * 60)
+print(dt)
 
 
 //: [Next](@next)
