c2a758e7bfeaba1ca93dea01556cb1ed1535abbc
frozenxnow <frozenxnow@naver.com>
2021-05-29 22:56:59 +0900

    2021-05-29

--- Mastering-iOS-master/xcode12+/CollectionView/CollectionView/Selection/SelectionViewController.swift
+++ Mastering-iOS-master/xcode12+/CollectionView/CollectionView/Selection/SelectionViewController.swift
@@ -39,12 +39,23 @@ class SelectionViewController: UIViewController {
     
     
     func selectRandomItem() {
+        let item = Int(arc4random_uniform(UInt32(list.count)))
+        let targetIndexPath = IndexPath(item: item, section: 0)
         
+        listCollectionView.selectItem(at: targetIndexPath, animated: true, scrollPosition: .centeredHorizontally)
     }
     
     
     func reset() {
+        // 모든 선택을 해제한다
+//        선택된 셀을 모두 해제하는 메서드, 효율적이지 않다
+//        listCollectionView.deselectItem(at: <#T##IndexPath#>, animated: <#T##Bool#>)
         
+        // 위에서 사용했던 메서드에 nil을 전달해서 해제하는 방법도 있다
+        listCollectionView.selectItem(at: nil, animated: true, scrollPosition: .left)
+        // 스크롤 포지션을 전달해도 스크롤되지 않는다, 스크롤 초기화가 필요하면 직접 스크롤해야한다.
+        let firstIndexPath = IndexPath(item: 0, section: 0)
+        listCollectionView.scrollToItem(at: firstIndexPath, at: .left, animated: true)
     }
     
     
@@ -54,7 +65,7 @@ class SelectionViewController: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(showMenu))
                 
         listCollectionView.allowsSelection = true
-        listCollectionView.allowsMultipleSelection = true
+        listCollectionView.allowsMultipleSelection = false
         
     }
 }
@@ -65,21 +76,68 @@ extension SelectionViewController: UICollectionViewDelegate {
         let color = list[indexPath.item].color
         view.backgroundColor = color
         print("#1", indexPath, #function)
+        
+//        if let cell = collectionView.cellForItem(at: indexPath) {
+//            // 태그를 사용해 이미지를 가져온다 태그값은 100으로 설정되어 있음
+//            if let imageView = cell.contentView.viewWithTag(100) as? UIImageView {
+//                imageView.image = checkImage
+//            }
+//        }
     }
     
     func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        
         // cell 선택 직전에 호출 true: 선택, false: 선택되지 않음
         // 조건에 따라 선택할지 안할지에 대한 검증
         print("#2", indexPath, #function)
-        
         // 선택되어 있는 셀은 indexPathForSelectedItem으로 확인 : nil 이라면 true 리턴
         guard let list = collectionView.indexPathsForSelectedItems else {
             return true
         }
-        
 //        indexPath가 존재하지 않는 경우에만 true 리턴
         return !list.contains(indexPath)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, shouldDeselectItemAt indexPath: IndexPath) -> Bool {
+        // 선택된 셀 선택해제 전 호출, ture 선택해제, false 선택해제되지 않음
+        return true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        // 실제로 선택이 해제된 후 호출 : 선택해제 후 ui 업데이트에 활용
+        
+//        if let cell = collectionView.cellForItem(at: indexPath) {
+//            // 태그를 사용해 이미지를 가져온다 태그값은 100으로 설정되어 있음
+//            if let imageView = cell.contentView.viewWithTag(100) as? UIImageView {
+//                imageView.image = nil
+//                // 선택이 해제되면 체크 이미지가 삭제된다
+//            }
+//        }
+    }
+    
+    
+    // highlight를 처리할수 있는 세가지 메서드가 있다
+    func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        print("#3", indexPath, #function)
+        return true // true 하이라이트 강조 false는 강조하지 않음, 셀 선택도 되지 않는다
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        // 하이라이트 강조 후 호출
+        print("#4", indexPath, #function)
+        if let cell = collectionView.cellForItem(at: indexPath) {
+            cell.layer.borderWidth = 6
+        }
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        // 하이라이트 강조 해제시 호출
+        print("#5", indexPath, #function)
+        
+        if let cell = collectionView.cellForItem(at: indexPath) {
+            cell.layer.borderWidth = 0
+        }
     }
 }
 
