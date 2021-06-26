b680ef64b2fb790cb6e01540bac3286047ff85dd
frozenxnow <frozenxnow@naver.com>
2021-06-27 00:38:07 +0900

    2021-06-27

--- Mastering-iOS-master/old-xcode9+/13-AutoLayout/AdaptiveLayout/AdaptiveLayout/ViewController.swift
+++ Mastering-iOS-master/old-xcode9+/13-AutoLayout/AdaptiveLayout/AdaptiveLayout/ViewController.swift
@@ -25,7 +25,22 @@ import UIKit
 class ViewController: UIViewController {
 
    @IBOutlet weak var contentTextView: UITextView!
-   
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.willTransition(to: newCollection, with: coordinator)
+        
+        switch (newCollection.horizontalSizeClass, newCollection.verticalSizeClass) {
+        case (.regular, .regular):
+            contentTextView.font = UIFont.systemFont(ofSize: 30)
+        default:
+            contentTextView.font = UIFont.systemFont(ofSize: 20)
+        }
+        
+        print(#function)
+    }
+    
+    
    
 }
+
 
