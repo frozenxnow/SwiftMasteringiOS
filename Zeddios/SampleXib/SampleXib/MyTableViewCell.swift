6d004dbdb4eb7d32f365616d287f542815e82c0e
frozenxnow <frozenxnow@naver.com>
2021-08-11 23:26:30 +0900

    2021-08-11

---
+++ Zeddios/SampleXib/SampleXib/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  SampleXib
+//
+//  Created by 지원 on 2021/08/11.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Zeddios/SampleXib/SampleXib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleXib
+//
+//  Created by 지원 on 2021/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
